cc389f483f3a6d577c6735f54079f49bb93e8585
Jonathan Nilsson <lord.jonil@gmail.com>
2022-03-29 01:36:32 +0200

    Basic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use xcb::{x, Xid};
+
 mod egl;
 mod gl;
 
@@ -18,8 +20,6 @@ static CONFIG_ATTRIBS: &[i32] = &[
 static CONTEXT_ATTRIBS: &[i32] = &[egl::EGL_CONTEXT_CLIENT_VERSION, 3, egl::EGL_NONE];
 
 fn main() {
-    println!("Test ");
-
     let display = egl::get_display(egl::EGL_DEFAULT_DISPLAY).unwrap();
 
     dbg!(display);
@@ -42,9 +42,50 @@ fn main() {
 
     dbg!(context);
 
-    let surface = egl::create_window_surface(display, config, gbmSurface, &[]).unwrap();
+    let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
+
+    let setup = conn.get_setup();
+    let screen = setup.roots().nth(screen_num as usize).unwrap();
+
+    let window: x::Window = conn.generate_id();
+
+    let cookie = conn.send_request_checked(&x::CreateWindow {
+        depth: x::COPY_FROM_PARENT as u8,
+        wid: window,
+        parent: screen.root(),
+        x: 0,
+        y: 0,
+        width: 1280,
+        height: 720,
+        border_width: 0,
+        class: x::WindowClass::InputOutput,
+        visual: screen.root_visual(),
+        // this list must be in same order than `Cw` enum order
+        value_list: &[
+            x::Cw::BackPixel(screen.black_pixel()),
+            x::Cw::EventMask(x::EventMask::EXPOSURE | x::EventMask::KEY_PRESS),
+        ],
+    });
+
+    conn.check_request(cookie).unwrap();
+
+    conn.send_request(&x::MapWindow { window });
+
+    let surface =
+        egl::create_window_surface(display, config, window.resource_id() as _, &[]).unwrap();
 
     assert!(egl::make_current(display, surface, surface, context));
+
+    gl::load_with(|s| egl::get_proc_address(s) as _);
+
+    loop {
+        unsafe {
+            gl::ClearColor(1.0, 0.0, 0.0, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+        }
+
+        egl::swap_buffers(display, surface);
+    }
 
     assert!(egl::terminate(display));
 }
