9c4b5205fa61f7bb7a37c665c7cf3394bf6ce589
Jonathan Nilsson <lord.jonil@gmail.com>
2022-03-29 01:04:25 +0200

    Config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
 mod egl;
 mod gl;
 
+static CONFIG_ATTRIBS: &[i32] = &[
+    egl::EGL_RED_SIZE,
+    8,
+    egl::EGL_GREEN_SIZE,
+    8,
+    egl::EGL_BLUE_SIZE,
+    8,
+    egl::EGL_DEPTH_SIZE,
+    8,
+    egl::EGL_RENDERABLE_TYPE,
+    egl::EGL_OPENGL_ES3_BIT,
+    egl::EGL_NONE,
+];
+
+static CONTEXT_ATTRIBS: &[i32] = &[egl::EGL_CONTEXT_CLIENT_VERSION, 2, egl::EGL_NONE];
+
 fn main() {
     println!("Test ");
+
+    let display = egl::get_display(egl::EGL_DEFAULT_DISPLAY).unwrap();
+
+    dbg!(display);
+
+    let mut major = 0;
+    let mut minor = 0;
+
+    assert!(egl::initialize(display, &mut major, &mut minor));
+
+    dbg!(major, minor);
+
+    assert!(egl::bind_api(egl::EGL_OPENGL_ES_API));
+
+    let config = egl::choose_config(display, CONFIG_ATTRIBS, 1).unwrap();
+
+    dbg!(config);
 }
