20cda7833784af32bd79ee489b99e0f2122dfac5
Jonathan Nilsson <lord.jonil@gmail.com>
2022-03-29 01:11:20 +0200

    Where is a window

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ static CONFIG_ATTRIBS: &[i32] = &[
     egl::EGL_NONE,
 ];
 
-static CONTEXT_ATTRIBS: &[i32] = &[egl::EGL_CONTEXT_CLIENT_VERSION, 2, egl::EGL_NONE];
+static CONTEXT_ATTRIBS: &[i32] = &[egl::EGL_CONTEXT_CLIENT_VERSION, 3, egl::EGL_NONE];
 
 fn main() {
     println!("Test ");
@@ -36,4 +36,15 @@ fn main() {
     let config = egl::choose_config(display, CONFIG_ATTRIBS, 1).unwrap();
 
     dbg!(config);
+
+    let context =
+        egl::create_context(display, config, egl::EGL_NO_CONTEXT, CONTEXT_ATTRIBS).unwrap();
+
+    dbg!(context);
+
+    let surface = egl::create_window_surface(display, config, gbmSurface, &[]).unwrap();
+
+    assert!(egl::make_current(display, surface, surface, context));
+
+    assert!(egl::terminate(display));
 }
