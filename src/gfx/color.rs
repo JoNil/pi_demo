9d220adeb1e2bb7ce1c46dbfa01453657b0fffdf
Jonathan Nilsson <lord.jonil@gmail.com>
2022-03-29 22:15:06 +0200

    Cleanup

--- src/gfx/color.rs
+++ src/gfx/color.rs
@@ -1,13 +1,8 @@
-/// Represents a visual color
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Color {
-    /// Red value
     pub r: f32,
-    /// Green value
     pub g: f32,
-    /// Blue value
     pub b: f32,
-    /// Alpha value
     pub a: f32,
 }
 
@@ -32,19 +27,16 @@ impl Color {
     pub const PINK: Color = Color::new(1.0, 0.753, 0.796, 1.0);
 
     #[inline(always)]
-    /// Create a new color from red, green, blue and alpha values
     pub const fn new(r: f32, g: f32, b: f32, a: f32) -> Self {
         Self { r, g, b, a }
     }
 
     #[inline(always)]
-    /// Create a new color from red, green, blue and alpha values
     pub const fn from_rgba(r: f32, g: f32, b: f32, a: f32) -> Self {
         Self::new(r, g, b, a)
     }
 
     #[inline(always)]
-    /// Create a new color from red, green and blue values
     pub const fn from_rgb(r: f32, g: f32, b: f32) -> Self {
         Self::from_rgba(r, g, b, 1.0)
     }
@@ -57,7 +49,6 @@ impl Color {
     }
 
     #[inline(always)]
-    /// Create a new color from rgba bytes
     pub fn from_bytes(r: u8, g: u8, b: u8, a: u8) -> Self {
         Self {
             r: r as f32 / 255.0,
@@ -68,37 +59,31 @@ impl Color {
     }
 
     #[inline(always)]
-    /// Returns the same color with the red passed
     pub const fn with_red(&self, red: f32) -> Color {
         Self::new(red, self.g, self.b, self.a)
     }
 
     #[inline(always)]
-    /// Returns the same color with the green passed
     pub const fn with_green(&self, green: f32) -> Color {
         Self::new(self.r, green, self.b, self.a)
     }
 
     #[inline(always)]
-    /// Returns the same color with the blue passed
     pub const fn with_blue(&self, blue: f32) -> Color {
         Self::new(self.r, self.g, blue, self.a)
     }
 
     #[inline(always)]
-    /// Returns the same color with the alpha passed
     pub const fn with_alpha(&self, alpha: f32) -> Color {
         Self::new(self.r, self.g, self.b, alpha)
     }
 
     #[inline(always)]
-    /// Returns an array with the r, g, b, a values
     pub const fn rgba(&self) -> [f32; 4] {
         [self.r, self.g, self.b, self.a]
     }
 
     #[inline(always)]
-    /// Returns an array with the r, g, b values
     pub const fn rgb(&self) -> [f32; 3] {
         [self.r, self.g, self.b]
     }
@@ -116,7 +101,6 @@ impl Color {
     }
 
     #[inline(always)]
-    /// Returns byte representation of the color
     pub fn rgba_u8(&self) -> [u8; 4] {
         let r = (self.r * 255.0) as _;
         let g = (self.g * 255.0) as _;
@@ -126,19 +110,12 @@ impl Color {
     }
 
     #[inline(always)]
-    /// Returns the same color as premultiplied alpha
     pub fn to_premultiplied_alpha(&self) -> Color {
-        if self.a == 0.0 {
-            Self::TRANSPARENT
-        } else if self.a == 1.0 {
-            *self
-        } else {
-            Self {
-                r: self.r * self.a,
-                g: self.g * self.a,
-                b: self.b * self.a,
-                a: self.a,
-            }
+        Self {
+            r: self.r * self.a,
+            g: self.g * self.a,
+            b: self.b * self.a,
+            a: self.a,
         }
     }
 }
@@ -190,7 +167,6 @@ impl std::fmt::Display for Color {
 }
 
 #[inline(always)]
-/// Converts a rgba color values to a hexadecimal values
 pub fn rgba_to_hex(r: f32, g: f32, b: f32, a: f32) -> u32 {
     (((r * 255.0) as u32) << 24)
         + (((g * 255.0) as u32) << 16)
@@ -199,7 +175,6 @@ pub fn rgba_to_hex(r: f32, g: f32, b: f32, a: f32) -> u32 {
 }
 
 #[inline(always)]
-/// Converts an hexadecimal value to a rgba values
 pub fn hex_to_rgba(hex: u32) -> [f32; 4] {
     [
         ((hex >> 24) & 0xFF) as f32 / 255.0,
@@ -210,7 +185,6 @@ pub fn hex_to_rgba(hex: u32) -> [f32; 4] {
 }
 
 #[inline(always)]
-/// Converts a hexadecimal value to string
 pub fn hex_to_string(hex: u32) -> String {
     format!("{:#X}", hex)
 }
