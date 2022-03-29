b624d29bbd30132508d1d919a29c4962ab776416
Jonathan Nilsson <lord.jonil@gmail.com>
2022-03-29 02:01:37 +0200

    Some cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    platform::unix::WindowExtUnix,
+    platform::{run_return::EventLoopExtRunReturn, unix::WindowExtUnix},
     window::WindowBuilder,
 };
 
@@ -47,7 +47,7 @@ fn main() {
 
     dbg!(context);
 
-    let event_loop = EventLoop::new();
+    let mut event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
     let surface =
@@ -58,19 +58,21 @@ fn main() {
 
     gl::load_with(|s| egl::get_proc_address(s) as _);
 
-    loop {
-        unsafe {
-            gl::ClearColor(1.0, 1.0, 0.0, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
-        }
-
-        egl::swap_buffers(display, surface);
-    }
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+    event_loop.run_return(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
 
         match event {
+            Event::RedrawRequested(_) => {
+                unsafe {
+                    gl::ClearColor(1.0, 1.0, 0.0, 1.0);
+                    gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+                }
+
+                egl::swap_buffers(display, surface);
+            }
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 window_id,
@@ -79,5 +81,7 @@ fn main() {
         }
     });
 
+    assert!(egl::destroy_surface(display, surface));
+    assert!(egl::destroy_context(display, context));
     assert!(egl::terminate(display));
 }
