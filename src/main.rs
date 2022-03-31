14cf2f41d66851a4c3fbc93ce578328aacf8f526
Jonathan Nilsson <lord.jonil@gmail.com>
2022-04-01 00:09:14 +0200

    Equilateral triangle

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use crate::{
     },
     gfx_backend::GlesBackend,
 };
+use glam::Mat4;
+use std::f32::consts::PI;
 use winit::{
     event::Event,
     event_loop::{ControlFlow, EventLoop},
@@ -19,14 +21,18 @@ mod gfx_backend;
 
 const VERT: &str = r#"
     #version 310 es
-    layout(location = 0) in vec2 a_pos;
+    layout(location = 0) in vec3 a_pos;
     layout(location = 1) in vec3 a_color;
 
     layout(location = 0) out vec3 v_color;
+    
+    layout(binding = 0) uniform Locals {
+        mat4 u_mvp;
+    };
 
     void main() {
         v_color = a_color;
-        gl_Position = vec4(a_pos - 0.5, 0.0, 1.0);
+        gl_Position = u_mvp * vec4(a_pos, 1.0);
     }
 "#;
 
@@ -51,7 +57,7 @@ fn main() {
     let clear_options = ClearOptions::color(Color::new(0.1, 0.2, 0.3, 1.0));
 
     let vertex_info = VertexInfo::new()
-        .attr(0, VertexFormat::Float32x2)
+        .attr(0, VertexFormat::Float32x3)
         .attr(1, VertexFormat::Float32x3);
 
     let pipeline = device
@@ -63,9 +69,9 @@ fn main() {
 
     #[rustfmt::skip]
     let vertices = [
-        0.5, 1.0,   1.0, 0.2, 0.3,
-        0.0, 0.0,   0.1, 1.0, 0.3,
-        1.0, 0.0,   0.1, 0.2, 1.0,
+         0.0,    0.5,  -1.0,   1.0, 0.2, 0.3,
+         0.433, -0.25, -1.0,   0.1, 1.0, 0.3,
+        -0.433, -0.25, -1.0,   0.1, 0.2, 1.0,
     ];
 
     let vbo = device
@@ -75,6 +81,10 @@ fn main() {
         .build()
         .unwrap();
 
+    let uniform_buffer = device.create_uniform_buffer(0, "Locals").build().unwrap();
+
+    let mut angle = 0.0;
+
     event_loop.run_return(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
 
@@ -82,9 +92,24 @@ fn main() {
             Event::RedrawRequested(_) => {
                 let mut encoder = device.create_command_encoder();
 
+                let mvp = Mat4::from_rotation_z(angle);
+                let proj = Mat4::perspective_rh_gl(
+                    PI / 2.0,
+                    device.size().0 as f32 / device.size().1 as f32,
+                    0.01,
+                    1000.0,
+                );
+
+                let mat = proj * mvp;
+
+                angle += 0.005;
+
+                device.set_buffer_data(&uniform_buffer, &mat.to_cols_array());
+
                 encoder.begin(Some(&clear_options));
                 encoder.set_pipeline(&pipeline);
                 encoder.bind_buffer(&vbo);
+                encoder.bind_buffer(&uniform_buffer);
                 encoder.draw(0, 3);
                 encoder.end();
 
